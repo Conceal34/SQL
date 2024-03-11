2faa23fd2bbdfae5673e35b18c0e680f11259ecc
Conceal34 <104488194+Conceal34@users.noreply.github.com>
2024-03-11 09:21:59 +0530

    Update library management system.sql

--- library management system.sql
+++ library management system.sql
@@ -7,7 +7,8 @@ create table authors (
 
 create table books (
   book_id varchar(100),
-  book_name varchar(100),
+  genre text,
+  title varchar(100),
   author_id varchar(100),
   book_qty int,
   primary key (book_id),
@@ -33,14 +34,14 @@ insert into authors values
   ('a006', 'Puneet');
   
 insert into books values
-  ('b101', 'Alice in wonderland', 'a001', 12),
-  ('b102', 'Mind mapping', 'a002', 3),
-  ('b103', 'learning habits', 'a003', 6),
-  ('b104', '12 rules of power', 'a004', 7),
-  ('b105', 'Atomic habits', 'a005', 2),
-  ('b106', 'Power of habits', 'a006', 1),
-  ('b107', 'Everybody around you is an idiot', 'a002', 10),
-  ('b108', 'Surrounded by geniuses', 'a005', 3);
+  ('b101', 'Fiction','Alice in wonderland', 'a001', 12),
+  ('b102', 'Mystery', 'Mind mapping', 'a002', 3),
+  ('b103', 'Habits', 'learning habits', 'a003', 6),
+  ('b104', 'Self help', '12 rules of power', 'a004', 7),
+  ('b105', 'Habits', 'Atomic habits', 'a005', 2),
+  ('b106', 'Habits', 'Power of habits', 'a006', 1),
+  ('b107', 'Fiction', 'Everybody around you is an idiot', 'a002', 10),
+  ('b108', 'Self help','Surrounded by geniuses', 'a005', 3);
   
 insert into borrowers values 
   ('b001', 'Aayush', 'b101', '2024-2-12', '2024-2-18'),
@@ -48,9 +49,22 @@ insert into borrowers values
   ('b003', 'Arman', 'b101', '2024-2-18', '2024-2-25'),
   ('b004', 'Vinayak', 'b101', '2024-1-30', '2024-2-17'),
   ('b005', 'Sahil', 'b101', '2024-2-18', '2024-3-10');
-  
-select * from borrowers;
-select * from books;
-select * from authors;
+
+select books.title, authors.author_name
+from books
+inner join authors 
+on books.author_id = authors.author_id
+where genre = 'Habits';
+
+
+
+select * from books
+order by book_qty desc;
+
+
+
+-- select * from borrowers;
+-- select * from books;
+-- select * from authors;
 
 
